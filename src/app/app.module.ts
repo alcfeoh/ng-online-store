d8efb394b6ae7084bf8b2a3f23266b2c12beca29
Alain Chautard <achautard@gmail.com>
2017-12-05 11:44:35 -0800

    Switched to Bootstrap instead of Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-
 import { AppComponent } from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {
-  MatButtonModule, MatCardModule, MatGridListModule, MatIconModule, MatListModule, MatSidenavModule,
-  MatToolbarModule
-} from '@angular/material';
 
 
 @NgModule({
@@ -16,16 +9,7 @@ import {
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatToolbarModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatButtonModule,
-    MatListModule,
-    MatGridListModule,
-    MatCardModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
